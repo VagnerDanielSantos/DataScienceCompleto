1b653be1c13361682e339b1a4c9369658a5016a0
VagnerDanielSantos <vagner.daniel.santoss@gmail.com>
2022-06-13 16:11:51 -0300

    Primeiro Arquivo do Capitulo 12

---
+++ BigDataAnalyticsComR/Capitulo12/02-Exercicios--Parte02.R
@@ -0,0 +1,62 @@
+
+# Exercícios - Capítulo 12
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+
+setwd("D:/BigDataAnalytics/Capitulo12/Exercicios")
+getwd()
+
+# Usaremos o dataset iris neste exemplo
+# O dataset iris possui observações de 3 especies de flores (Iris setosa, Iris virginica e Iris versicolor)
+# Para cada flor, 4 medidas sao usadas: 
+# comprimento (length) e largura (width) do caule (sepal) e comprimento e largura da petala (petal)
+
+library(datasets)
+View(iris)
+
+# Análise exploratoria de dados com ggplot2
+
+install.packages("ggplot2")
+library(ggplot2)
+
+# Veja que os dados claramente possui grupos com caracteristcas similares
+
+ggplot(iris, aes(Petal.Length, Petal.Width, color = Species)) + geom_point(size = 3)
+
+# Agora usarmeos o K-Means para tentar agrupar os dados em clusters
+
+help(kmeans)
+
+# Exercício 01 - Usando a função kmeans(), 
+# crie um modelo de clustering (aprendizagem não supervisionada). 
+# Use a documentação, para fazer sua pesquisa.
+# Neste caso, ja sabemos quantos grupos (clusters) existem em nossos dados (3)
+# Perceba que o dataset iris possui 5 colunas, mas estamos usando as 4 primeiras
+
+iris_kmeans <- kmeans(iris[ , 1:4], centers = 3, nstart = 21)
+iris_kmeans
+
+# Obtendo informação sobre os clusters
+# Foram criados 3 clusters: cluster 1, 2 e 3
+# Perceba que apesar o algoritmo ter feito a divisão dos dados em clusters, 
+# houve problema em dividir alguns dos dados, que apesar de terem caracteristicas 
+# diferentes, ficaram no mesmo cluster
+
+table(iris_kmeans$cluster, iris$Species)
+iris_kmeans
+
+# Visualizando os clusters
+
+install.packages("cluster")
+library(cluster)
+
+# Plot
+
+help(clusplot)
+
+clusplot(iris, iris_kmeans$cluster, color = TRUE, shade = TRUE, labels = 0, lines = 0, )
